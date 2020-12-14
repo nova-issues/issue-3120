0393a7437ee31f5af625426cf7b863ed50a9d8fe
Mior Muhammad Zaki <crynobone@gmail.com>
2020-12-14 09:29:16 +0800

    Add reproducing code for laravel/nova-issues#3120
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

---
+++ app/Nova/Lenses/Users.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Nova\Lenses;
+
+use App\Nova\Actions\MarkAsActive;
+use App\Nova\Actions\MarkAsInactive;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Boolean;
+use Laravel\Nova\Fields\Gravatar;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\ActionRequest;
+use Laravel\Nova\Http\Requests\LensRequest;
+use Laravel\Nova\Lenses\Lens;
+
+class Users extends Lens
+{
+    /**
+     * Get the query builder / paginator for the lens.
+     *
+     * @param  \Laravel\Nova\Http\Requests\LensRequest  $request
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return mixed
+     */
+    public static function query(LensRequest $request, $query)
+    {
+        return $request->withOrdering($request->withFilters(
+            $query
+        ));
+    }
+
+    /**
+     * Get the fields available to the lens.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request)
+    {
+        return [
+            ID::make(__('ID'), 'id')->sortable(),
+
+            Gravatar::make()->maxWidth(50),
+
+            Text::make('Name')->sortable(),
+
+            Text::make('Email')->sortable(),
+
+            Boolean::make('Active'),
+        ];
+    }
+
+    /**
+     * Get the cards available on the lens.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the lens.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available on the lens.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request)
+    {
+        return [
+            new MarkAsActive,
+            MarkAsInactive::make()->showOnTableRow(),
+        ];
+    }
+
+    /**
+     * Get the URI key for the lens.
+     *
+     * @return string
+     */
+    public function uriKey()
+    {
+        return 'users';
+    }
+}
